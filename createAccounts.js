36b01467a5dc01fc6a786c4dcbacad7978e350ee
Utkarsh Barsaiyan <ubarsaiyan@gmail.com>
2018-10-10 00:31:06 +0530

    Make tests framework independent

--- createAccounts.js
+++ createAccounts.js
@@ -1,11 +1,13 @@
 const admin = require('./admin');
 const credentials = require('./credentials');
 const generator = require('generate-password');
+require('dotenv').config();
+require('./richmessage.js');
 
 const uId = 'test';
 const rnd = generator.generate({ length: 3, numbers: true });
 credentials(uId).setEmail(`test${rnd}@test.com`);
-credentials(uId).setFramework('bbot');
+credentials(uId).setFramework(process.env.BOT_FRAMEWORK);
 credentials(uId).generateUsername();
 credentials(uId).generatePassword();
 const cred = credentials(uId).generateRoom();
@@ -15,3 +17,9 @@ const fs = require('file-system');
 fs.writeFile('credentials.json', json, 'utf8');
 
 admin.createAccounts(cred.toObject());
+setTimeout(function () { //TODO: Replace setTimeout by a callback function
+    if (process.env.BOT_FRAMEWORK === 'bbot') {
+        const bBot = require('bbot');
+        bBot.start();
+    }
+}, 5000);
